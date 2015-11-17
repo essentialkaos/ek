7237e6bc8668f5697517f52c9bffb064349ecbf1
Anton Novojilov <andy@essentialkaos.com>
2015-11-17 15:00:30 +0100

    Improved test script

--- .travis/script.sh
+++ .travis/script.sh
@@ -11,6 +11,8 @@ main() {
 
   local pkg has_errors
 
+  local count=0
+
   rm -f coverage.tmp coverage.txt &> /dev/null
 
   for pkg in $(ls -1 $dir) ; do
@@ -18,21 +20,42 @@ main() {
       continue
     fi
 
-    go test -coverprofile=coverage.tmp -covermode=atomic $dir/$pkg
+    while : ; do
+      testPackage "$dir" "$pkg"
 
-    if [[ $? -ne 0 ]] ; then
-      has_errors=true
-    fi
+      if [[ $? -ne 0 ]] ; then
+        # Workaround for perediocal errors on TravisCI containers
+        if [[ "$pkg" == "req" && $count -ne 3 ]] ; then
+          ((count++))
+          continue
+        else
+          has_errors=true
+          break
+        fi
+      fi
+
+      break
+    done
 
     if [[ -f coverage.tmp ]] ; then
       cat coverage.tmp >> coverage.txt
-      rm -f coverage.tmp
     fi
   done
 
   if [[ $has_errors ]] ; then
     exit 1
   fi
+}
+
+testPackage() {
+  local dir="$1"
+  local pkg="$2"
+
+  rm -f coverage.tmp
+
+  go test -coverprofile=coverage.tmp -covermode=atomic $dir/$pkg
+
+  return $?
 }
 
 ########################################################################################
