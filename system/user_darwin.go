90e9d5ec80517900f51a0de76fdea487fcf90e47
Anton Novojilov <andy@essentialkaos.com>
2015-12-24 01:56:14 +0100

    Added fix for getting info about user on mac

--- system/user_darwin.go
+++ system/user_darwin.go
@@ -11,6 +11,8 @@ package system
 
 import (
 	"errors"
+	"strconv"
+	"strings"
 	"syscall"
 	"time"
 )
@@ -35,4 +37,61 @@ func getTimes(path string) (time.Time, time.Time, time.Time, error) {
 		time.Unix(int64(stat.Mtimespec.Sec), int64(stat.Mtimespec.Nsec)),
 		time.Unix(int64(stat.Ctimespec.Sec), int64(stat.Ctimespec.Nsec)),
 		nil
+}
+
+// getUserInfo return user info by name (name, id, gid, comment, home, shell)
+//
+func getUserInfo(nameOrID string) (string, int, int, string, string, string, error) {
+	cmd := exec.Command("dscl", ".", "-read", "/Users/"+nameOrID)
+
+	out, err := cmd.Output()
+
+	if err != nil || len(out) == 0 {
+		return "", -1, -1, "", "", "", fmt.Errorf("User with this name/id %s is not exist", nameOrID)
+	}
+
+	var (
+		lineStart = 0
+		uid       int
+		gid       int
+		home      string
+		shell     string
+	)
+
+	for i, b := range out {
+		if b != '\n' {
+			continue
+		}
+
+		// Skip long lines
+		if i-lineStart > 128 {
+			lineStart = i + 1
+			continue
+		}
+
+		line := string(out[lineStart:i])
+
+		lineStart = i + 1
+
+		sepIndex := strings.Index(line, ":")
+
+		if sepIndex == -1 {
+			continue
+		}
+
+		rec := line[0:sepIndex]
+
+		switch rec {
+		case "UniqueID":
+			uid, _ = strconv.Atoi(line[sepIndex+2:])
+		case "PrimaryGroupID":
+			gid, _ = strconv.Atoi(line[sepIndex+2:])
+		case "NFSHomeDirectory":
+			home = line[sepIndex+2:]
+		case "UserShell":
+			shell = line[sepIndex+2:]
+		}
+	}
+
+	return nameOrID, uid, gid, "", home, shell, nil
 }
--- system/user_linux.go
+++ system/user_linux.go
@@ -36,3 +36,24 @@ func getTimes(path string) (time.Time, time.Time, time.Time, error) {
 		time.Unix(int64(stat.Ctim.Sec), int64(stat.Ctim.Nsec)),
 		nil
 }
+
+// getUserInfo return user info by name or id (name, id, gid, comment, home, shell)
+//
+func getUserInfo(nameOrID string) (string, int, int, string, string, string, error) {
+	cmd := exec.Command("getent", "passwd", nameOrID)
+
+	out, err := cmd.Output()
+
+	if err != nil {
+		return "", -1, -1, "", "", "", fmt.Errorf("User with this name/id %s is not exist", nameOrID)
+	}
+
+	sOut := string(out[:])
+	sOut = strings.Trim(sOut, "\n")
+	aOut := strings.Split(sOut, ":")
+
+	uid, _ := strconv.Atoi(aOut[2])
+	gid, _ := strconv.Atoi(aOut[3])
+
+	return aOut[0], uid, gid, aOut[4], aOut[5], aOut[6], nil
+}
