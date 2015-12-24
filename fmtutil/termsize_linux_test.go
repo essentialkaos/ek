93f366a5f2495571f9b5309be4a2eb379b06c5ae
Anton Novojilov <andy@essentialkaos.com>
2015-12-24 10:40:17 +0100

    Fixed fmtutil tests on linux

---
+++ fmtutil/termsize_linux_test.go
@@ -0,0 +1,23 @@
+// +build linux
+
+package fmtutil
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+//                                                                                    //
+//                     Copyright (c) 2009-2015 Essential Kaos                         //
+//      Essential Kaos Open Source License <http://essentialkaos.com/ekol?en>         //
+//                                                                                    //
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+import (
+	. "gopkg.in/check.v1"
+)
+
+// ////////////////////////////////////////////////////
+
+func (s *FmtUtilSuite) TestTermSize(c *C) {
+	w, h := GetTermSize()
+
+	c.Assert(w, Not(Equals), 0)
+	c.Assert(h, Not(Equals), 0)
+}
