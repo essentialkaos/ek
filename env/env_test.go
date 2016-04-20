948cb3fecb657b501000294192694e9dc09c6c13
Anton Novojilov <andy@essentialkaos.com>
2016-04-20 05:45:12 -0400

    Improved code coverage for env package

--- env/env_test.go
+++ env/env_test.go
@@ -35,4 +35,11 @@ func (s *ENVSuite) TestEnv(c *C) {
 	c.Assert(envs.GetS("EK_TEST_PORT"), Equals, "8080")
 	c.Assert(envs.GetI("EK_TEST_PORT"), Equals, 8080)
 	c.Assert(envs.GetF("EK_TEST_PORT"), Equals, 8080.0)
+
+	c.Assert(envs.GetS("UNKNOWN_VARIABLE"), Equals, "")
+	c.Assert(envs.GetI("UNKNOWN_VARIABLE"), Equals, -1)
+	c.Assert(envs.GetF("UNKNOWN_VARIABLE"), Equals, -1.0)
+
+	c.Assert(Which("cat"), Not(Equals), "")
+	c.Assert(Which("catABCD1234"), Equals, "")
 }
