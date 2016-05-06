668a49f58961f50b62ba906e0ede8b0f73ef9720
Anton Novojilov <andy@essentialkaos.com>
2016-05-06 09:20:30 -0400

    Fixed listing filtering bugs

--- fsutil/fs_test.go
+++ fsutil/fs_test.go
@@ -56,6 +56,8 @@ func (s *FSSuite) TestList(c *check.C) {
 	listing8 := ListAllFiles(tmpDir, true)
 	listing9 := ListAllFiles(tmpDir, true, &ListingFilter{MatchPatterns: []string{"*.mp3", "*.wav"}})
 	listing10 := ListAllFiles(tmpDir, true, &ListingFilter{NotMatchPatterns: []string{"*.mp3"}})
+	listing11 := List(tmpDir, true, &ListingFilter{Perms: "DR"})
+	listing12 := List(tmpDir, true, &ListingFilter{NotPerms: "DR"})
 
 	sort.Strings(listing1)
 	sort.Strings(listing2)
@@ -126,6 +128,18 @@ func (s *FSSuite) TestList(c *check.C) {
 		listing10,
 		check.DeepEquals,
 		[]string{"dir2/file4.wav", "file2.jpg"},
+	)
+
+	c.Assert(
+		listing11,
+		check.DeepEquals,
+		[]string{"dir2", "dir1"},
+	)
+
+	c.Assert(
+		listing12,
+		check.DeepEquals,
+		[]string{"file1.mp3", "file2.jpg"},
 	)
 }
 
