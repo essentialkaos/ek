96c44ecc8fbaaf478aa3f3ec03c7e3075cc472cf
Anton Novojilov <andy@essentialkaos.com>
2016-09-28 07:28:35 -0400

    [skip ci] More examples added

--- req/example_test.go
+++ req/example_test.go
@@ -14,8 +14,12 @@ import (
 // ////////////////////////////////////////////////////////////////////////////////// //
 
 func ExampleRequest_Do() {
+	var response = struct {
+		User    string  `json:"user"`
+		ID      int     `json:"id"`
+		Balance float64 `json:"balance"`
+	}{}
 
-	// Simple request
 	resp, err := Request{
 		Method: GET,
 		URL:    "https://my.domain.com",
@@ -32,9 +36,91 @@ func ExampleRequest_Do() {
 	}.Do()
 
 	if err != nil {
-		return
+		fmt.Printf("Error: %v\n", err)
 	}
 
 	// print status code
 	fmt.Printf("Status code: %d\n", resp.StatusCode)
+
+	// decode json encoded response
+	err = resp.JSON(response)
+
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+	}
+
+	// print response data
+	fmt.Printf(
+		"User: %s ID: %d Balance: %f\n",
+		response.User, response.ID, response.Balance,
+	)
+}
+
+func ExampleRequest_Get() {
+	var response = struct {
+		User    string  `json:"user"`
+		ID      int     `json:"id"`
+		Balance float64 `json:"balance"`
+	}{}
+
+	resp, err := Request{URL: "https://my.domain.com"}.Get()
+
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+	}
+
+	// decode json encoded response
+	err = resp.JSON(response)
+
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+	}
+
+	// print response data
+	fmt.Printf(
+		"User: %s ID: %d Balance: %f\n",
+		response.User, response.ID, response.Balance,
+	)
+}
+
+func ExampleRequest_Post() {
+	var request = struct {
+		UserID int `json:"user_id"`
+	}{
+		UserID: 1234,
+	}
+
+	var response = struct {
+		User    string  `json:"user"`
+		ID      int     `json:"id"`
+		Balance float64 `json:"balance"`
+	}{}
+
+	// send post request with basic auth
+	resp, err := Request{
+		URL:               "https://my.domain.com",
+		Body:              request,
+		Accept:            CONTENT_TYPE_JSON,
+		ContentType:       CONTENT_TYPE_JSON,
+		BasicAuthUsername: "someuser",
+		BasicAuthPassword: "somepass",
+		AutoDiscard:       true,
+	}.Post()
+
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+	}
+
+	// decode json encoded response
+	err = resp.JSON(response)
+
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+	}
+
+	// print response data
+	fmt.Printf(
+		"User: %s ID: %d Balance: %f\n",
+		response.User, response.ID, response.Balance,
+	)
 }
