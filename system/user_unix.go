02fdb3862758bce9970d8beb15b8d2eb57412e36
Anton Novojilov <andy@essentialkaos.com>
2017-04-14 07:28:35 -0400

    system package refactoring

--- system/user_unix.go
+++ system/user_unix.go
@@ -1,4 +1,4 @@
-// +build freebsd
+// +build linux freebsd
 
 package system
 
@@ -10,36 +10,13 @@ package system
 // ////////////////////////////////////////////////////////////////////////////////// //
 
 import (
-	"errors"
 	"fmt"
 	"os/exec"
 	"strconv"
 	"strings"
-	"syscall"
-	"time"
 )
 
 // ////////////////////////////////////////////////////////////////////////////////// //
-
-// getTimes is copy of fsutil.GetTimes
-func getTimes(path string) (time.Time, time.Time, time.Time, error) {
-	if path == "" {
-		return time.Time{}, time.Time{}, time.Time{}, errors.New("Path is empty")
-	}
-
-	var stat = &syscall.Stat_t{}
-
-	err := syscall.Stat(path, stat)
-
-	if err != nil {
-		return time.Time{}, time.Time{}, time.Time{}, err
-	}
-
-	return time.Unix(int64(stat.Atimespec.Sec), int64(stat.Atimespec.Nsec)),
-		time.Unix(int64(stat.Mtimespec.Sec), int64(stat.Mtimespec.Nsec)),
-		time.Unix(int64(stat.Ctimespec.Sec), int64(stat.Ctimespec.Nsec)),
-		nil
-}
 
 // getUserInfo find user info by name or id
 func getUserInfo(nameOrID string) (*User, error) {
