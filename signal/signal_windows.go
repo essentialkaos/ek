294e6a90776d38f5ad182ec69937131b28d1e4b3
Anton Novojilov <andy@essentialkaos.com>
2019-02-12 12:28:26 +0300

    Updated copyright header

--- signal/signal_windows.go
+++ signal/signal_windows.go
@@ -4,7 +4,7 @@ package signal
 
 // ////////////////////////////////////////////////////////////////////////////////// //
 //                                                                                    //
-//                     Copyright (c) 2009-2018 ESSENTIAL KAOS                         //
+//                     Copyright (c) 2009-2019 ESSENTIAL KAOS                         //
 //        Essential Kaos Open Source License <https://essentialkaos.com/ekol>         //
 //                                                                                    //
 // ////////////////////////////////////////////////////////////////////////////////// //
