c0512b784b1bc1ee4317e03a5ba9c1376e8ed358
Anton Novojilov <andy@essentialkaos.com>
2019-04-19 17:03:35 +0300

    Added examples for csv package

---
+++ csv/examples_test.go
@@ -0,0 +1,66 @@
+package csv
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+//                                                                                    //
+//                     Copyright (c) 2009-2019 ESSENTIAL KAOS                         //
+//        Essential Kaos Open Source License <https://essentialkaos.com/ekol>         //
+//                                                                                    //
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+func ExampleReader_Read() {
+	fd, err := os.Open("file.csv")
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	defer fd.Close()
+
+	reader := NewReader(fd)
+	reader.Comma = ','
+
+	for {
+		data, err := reader.Read()
+
+		if err == io.EOF {
+			break
+		}
+
+		fmt.Printf("%#v\n", data)
+	}
+}
+
+func ExampleReader_ReadTo() {
+	fd, err := os.Open("file.csv")
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	defer fd.Close()
+
+	reader := NewReader(fd)
+	reader.Comma = ','
+
+	data := make([]string, 10)
+
+	for {
+		err := reader.ReadTo(data)
+
+		if err == io.EOF {
+			break
+		}
+
+		fmt.Printf("%#v\n", data)
+	}
+}
