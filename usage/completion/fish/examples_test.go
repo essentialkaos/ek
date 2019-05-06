60eca57c4d18094884a6d152346be9ad1e86d033
Anton Novojilov <andy@essentialkaos.com>
2019-05-07 01:07:16 +0300

    Add usage examples for completion generators

---
+++ usage/completion/fish/examples_test.go
@@ -0,0 +1,34 @@
+package fish
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+//                                                                                    //
+//                     Copyright (c) 2009-2019 ESSENTIAL KAOS                         //
+//        Essential Kaos Open Source License <https://essentialkaos.com/ekol>         //
+//                                                                                    //
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+import (
+	"fmt"
+
+	"pkg.re/essentialkaos/ek.v10/usage"
+)
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+func ExampleGenerate() {
+	info := usage.NewInfo()
+
+	info.AddCommand("test", "Test data")
+	info.AddCommand("clean", "Clean data")
+
+	info.AddOption("d:dir", "Path to working directory", "dir")
+
+	info.BoundOptions("test", "d:dir")
+	info.BoundOptions("clean", "d:dir")
+
+	info.AddOption("nc:no-color", "Disable colors in output")
+	info.AddOption("h:help", "Show this help message")
+	info.AddOption("v:version", "Show version")
+
+	fmt.Println(Generate(info, "app"))
+}
---
+++ usage/completion/zsh/examples_test.go
@@ -0,0 +1,42 @@
+package zsh
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+//                                                                                    //
+//                     Copyright (c) 2009-2019 ESSENTIAL KAOS                         //
+//        Essential Kaos Open Source License <https://essentialkaos.com/ekol>         //
+//                                                                                    //
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+import (
+	"fmt"
+
+	"pkg.re/essentialkaos/ek.v10/options"
+	"pkg.re/essentialkaos/ek.v10/usage"
+)
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+func ExampleGenerate() {
+	info := usage.NewInfo()
+
+	info.AddCommand("test", "Test data")
+	info.AddCommand("clean", "Clean data")
+
+	info.AddOption("d:dir", "Path to working directory", "dir")
+
+	info.BoundOptions("test", "d:dir")
+	info.BoundOptions("clean", "d:dir")
+
+	info.AddOption("nc:no-color", "Disable colors in output")
+	info.AddOption("h:help", "Show this help message")
+	info.AddOption("v:version", "Show version")
+
+	opts := options.Map{
+		"d:dir":       {},
+		"nc:no-color": {Type: options.BOOL},
+		"h:help":      {Type: options.BOOL},
+		"v:version":   {Type: options.BOOL},
+	}
+
+	fmt.Println(Generate(info, opts, "app"))
+}
