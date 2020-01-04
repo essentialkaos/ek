f812b712321bf5e5f9726eee90065602205c9d59
Anton Novojilov <andy@essentialkaos.com>
2020-01-04 16:43:08 +0300

    Improvements

--- signal/signal.go
+++ signal/signal.go
@@ -11,6 +11,7 @@ package signal
 // ////////////////////////////////////////////////////////////////////////////////// //
 
 import (
+	"fmt"
 	"os"
 	"os/signal"
 	"strings"
@@ -114,133 +115,133 @@ func (h Handlers) TrackAsync() {
 // codebeat:disable[LOC,ABC]
 
 // GetByName returns signal with given name
-func GetByName(name string) os.Signal {
+func GetByName(name string) (os.Signal, error) {
 	switch strings.ToUpper(name) {
 	case "SIGABRT", "ABRT":
-		return ABRT
+		return ABRT, nil
 	case "SIGALRM", "ALRM":
-		return ALRM
+		return ALRM, nil
 	case "SIGBUS", "BUS":
-		return BUS
+		return BUS, nil
 	case "SIGCHLD", "CHLD":
-		return CHLD
+		return CHLD, nil
 	case "SIGCONT", "CONT":
-		return CONT
+		return CONT, nil
 	case "SIGFPE", "FPE":
-		return FPE
+		return FPE, nil
 	case "SIGHUP", "HUP":
-		return HUP
+		return HUP, nil
 	case "SIGILL", "ILL":
-		return ILL
+		return ILL, nil
 	case "SIGINT", "INT":
-		return INT
+		return INT, nil
 	case "SIGIO", "IO":
-		return IO
+		return IO, nil
 	case "SIGIOT", "IOT":
-		return IOT
+		return IOT, nil
 	case "SIGKILL", "KILL":
-		return KILL
+		return KILL, nil
 	case "SIGPIPE", "PIPE":
-		return PIPE
+		return PIPE, nil
 	case "SIGPROF", "PROF":
-		return PROF
+		return PROF, nil
 	case "SIGQUIT", "QUIT":
-		return QUIT
+		return QUIT, nil
 	case "SIGSEGV", "SEGV":
-		return SEGV
+		return SEGV, nil
 	case "SIGSTOP", "STOP":
-		return STOP
+		return STOP, nil
 	case "SIGSYS", "SYS":
-		return SYS
+		return SYS, nil
 	case "SIGTERM", "TERM":
-		return TERM
+		return TERM, nil
 	case "SIGTRAP", "TRAP":
-		return TRAP
+		return TRAP, nil
 	case "SIGTSTP", "TSTP":
-		return TSTP
+		return TSTP, nil
 	case "SIGTTIN", "TTIN":
-		return TTIN
+		return TTIN, nil
 	case "SIGTTOU", "TTOU":
-		return TTOU
+		return TTOU, nil
 	case "SIGURG", "URG":
-		return URG
+		return URG, nil
 	case "SIGUSR1", "USR1":
-		return USR1
+		return USR1, nil
 	case "SIGUSR2", "USR2":
-		return USR2
+		return USR2, nil
 	case "SIGVTALRM", "VTALRM":
-		return VTALRM
+		return VTALRM, nil
 	case "SIGWINCH", "WINCH":
-		return WINCH
+		return WINCH, nil
 	case "SIGXCPU", "XCPU":
-		return XCPU
+		return XCPU, nil
 	case "SIGXFSZ", "XFSZ":
-		return XFSZ
+		return XFSZ, nil
 	}
 
-	return syscall.Signal(-1)
+	return syscall.Signal(-1), fmt.Errorf("Unknown signal name %s", name)
 }
 
 // GetByCode returns signal with given code
-func GetByCode(code int) os.Signal {
+func GetByCode(code int) (os.Signal, error) {
 	switch code {
 	case 1:
-		return HUP
+		return HUP, nil
 	case 2:
-		return INT
+		return INT, nil
 	case 3:
-		return QUIT
+		return QUIT, nil
 	case 4:
-		return ILL
+		return ILL, nil
 	case 5:
-		return TRAP
+		return TRAP, nil
 	case 6:
-		return ABRT
+		return ABRT, nil
 	case 8:
-		return FPE
+		return FPE, nil
 	case 9:
-		return KILL
+		return KILL, nil
 	case 10:
-		return BUS
+		return BUS, nil
 	case 11:
-		return SEGV
+		return SEGV, nil
 	case 12:
-		return SYS
+		return SYS, nil
 	case 13:
-		return PIPE
+		return PIPE, nil
 	case 14:
-		return ALRM
+		return ALRM, nil
 	case 15:
-		return TERM
+		return TERM, nil
 	case 16:
-		return USR1
+		return USR1, nil
 	case 17:
-		return USR2
+		return USR2, nil
 	case 18:
-		return CHLD
+		return CHLD, nil
 	case 20:
-		return TSTP
+		return TSTP, nil
 	case 21:
-		return URG
+		return URG, nil
 	case 23:
-		return STOP
+		return STOP, nil
 	case 25:
-		return CONT
+		return CONT, nil
 	case 26:
-		return TTIN
+		return TTIN, nil
 	case 27:
-		return TTOU
+		return TTOU, nil
 	case 28:
-		return VTALRM
+		return VTALRM, nil
 	case 29:
-		return PROF
+		return PROF, nil
 	case 30:
-		return XCPU
+		return XCPU, nil
 	case 31:
-		return XFSZ
+		return XFSZ, nil
 	}
 
-	return syscall.Signal(-1)
+	return syscall.Signal(-1), fmt.Errorf("Unknown signal code %d", code)
 }
 
 // codebeat:enable[LOC,ABC]
