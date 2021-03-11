608998731aaaa3965ed35beda717768a60bb051b
Anton Novojilov <andy@essentialkaos.com>
2021-03-11 15:46:09 +0300

    Update year in copyright header

--- system/info_linux_arm.go
+++ system/info_linux_arm.go
@@ -5,7 +5,7 @@ package system
 
 // ////////////////////////////////////////////////////////////////////////////////// //
 //                                                                                    //
-//                         Copyright (c) 2020 ESSENTIAL KAOS                          //
+//                         Copyright (c) 2021 ESSENTIAL KAOS                          //
 //      Apache License, Version 2.0 <https://www.apache.org/licenses/LICENSE-2.0>     //
 //                                                                                    //
 // ////////////////////////////////////////////////////////////////////////////////// //
