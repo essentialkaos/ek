2aee394d4f1a8b0b3dcc1a7550d024c0afdc8858
Anton Novojilov <andy@essentialkaos.com>
2023-07-26 16:51:54 +0300

    [panel] Move panel rendering to it's own sub-package

--- fmtutil/panel/example_test.go
+++ fmtutil/panel/example_test.go
@@ -7,8 +7,8 @@ package panel
 //                                                                                    //
 // ////////////////////////////////////////////////////////////////////////////////// //
 
-func ExampleErrorPanel() {
-	ErrorPanel(
+func ExampleError() {
+	Error(
 		"Can't send data to remote server.",
 		`{*}Lorem ipsum{!} dolor sit amet, {r*}consectetur{!} adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
@@ -17,8 +17,8 @@ in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
 	)
 }
 
-func ExampleWarnPanel() {
-	WarnPanel(
+func ExampleWarn() {
+	Warn(
 		"Can't find user bob on system.",
 		`{*}Lorem ipsum{!} dolor sit amet, {r*}consectetur{!} adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
@@ -27,8 +27,8 @@ in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
 	)
 }
 
-func ExampleInfoPanel() {
-	InfoPanel(
+func ExampleInfo() {
+	Info(
 		"Auto-saving is enabled - data will be saved after editing.",
 		`{*}Lorem ipsum{!} dolor sit amet, {r*}consectetur{!} adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
