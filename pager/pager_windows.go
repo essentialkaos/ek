3e56950c9d6772a4c0a28f8094600dc305acb7db
Anton Novojilov <andy@essentialkaos.com>
2023-11-21 00:55:28 +0300

    [pager] Add Windows stubs

---
+++ pager/pager_windows.go
@@ -0,0 +1,33 @@
+// Package pager provides methods for pager setup (more/less)
+package pager
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+//                                                                                    //
+//                         Copyright (c) 2023 ESSENTIAL KAOS                          //
+//      Apache License, Version 2.0 <https://www.apache.org/licenses/LICENSE-2.0>     //
+//                                                                                    //
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+import "errors"
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+// DEFAULT is default pager command
+const DEFAULT = "more"
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+var ErrAlreadySet = errors.New("Pager already set")
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+// ❗ Setup set up pager for work. After calling this method, any data sent to Stdout and
+// Stderr (using fmt, fmtc, or terminal packages) will go to the pager.
+func Setup(pager string) error {
+	return nil
+}
+
+// ❗ Complete finishes pager work
+func Complete() {
+	return
+}
