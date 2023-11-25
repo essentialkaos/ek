f6e39795beddde916302fd0ea3135c0bba2f1994
Anton Novojilov <andy@essentialkaos.com>
2023-11-25 23:54:07 +0300

    [color] Fix shorthand hex generation for #FFF

--- color/color.go
+++ color/color.go
@@ -279,7 +279,7 @@ func (c Hex) ToWeb(caps bool) string {
 	}
 
 	// Generate shorthand color only for RGB
-	if c < 0xFFFFFF && k[0] == k[1] && k[2] == k[3] && k[4] == k[5] {
+	if c <= 0xFFFFFF && k[0] == k[1] && k[2] == k[3] && k[4] == k[5] {
 		k = k[0:1] + k[2:3] + k[4:5]
 	}
 
