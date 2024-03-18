6f2341f2afc3fda230522c0cf11638e5d07c55bb
Anton Novojilov <andy@essentialkaos.com>
2024-03-18 16:32:49 +0300

    [system/container] Add usage examples

---
+++ system/container/example_test.go
@@ -0,0 +1,26 @@
+package container
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+//                                                                                    //
+//                         Copyright (c) 2024 ESSENTIAL KAOS                          //
+//      Apache License, Version 2.0 <https://www.apache.org/licenses/LICENSE-2.0>     //
+//                                                                                    //
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+import (
+	"fmt"
+)
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+func ExampleGetEngine() {
+	fmt.Printf("Container engine: %s\n", GetEngine())
+}
+
+func ExampleIsContainer() {
+	if !IsContainer() {
+		fmt.Println("It's not containerized")
+	} else {
+		fmt.Printf("It's containerized using %s engine\n", GetEngine())
+	}
+}
