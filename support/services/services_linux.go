dbf6b3468082f3b2f3100a2764097a65d8cf7bfb
Anton Novojilov <andy@essentialkaos.com>
2024-04-29 00:27:51 +0300

    [support/services] Add package for collecting services info

---
+++ support/services/services_linux.go
@@ -0,0 +1,43 @@
+// Package services provides methods for collecting information about system services
+package services
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+//                                                                                    //
+//                         Copyright (c) 2024 ESSENTIAL KAOS                          //
+//      Apache License, Version 2.0 <https://www.apache.org/licenses/LICENSE-2.0>     //
+//                                                                                    //
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+import (
+	"github.com/essentialkaos/ek/v12/initsystem"
+	"github.com/essentialkaos/ek/v12/support"
+)
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+// Collect collect info about services
+func Collect(services ...string) []support.Service {
+	var result []support.Service
+
+	for _, s := range services {
+		service := support.Service{Name: s, Status: support.STATUS_UNKNOWN}
+
+		if initsystem.IsPresent(s) {
+			service.IsPresent = true
+			service.IsEnabled, _ = initsystem.IsEnabled(s)
+
+			isWorks, err := initsystem.IsWorks(s)
+
+			switch {
+			case err == nil && isWorks:
+				service.Status = support.STATUS_WORKS
+			case err == nil && !isWorks:
+				service.Status = support.STATUS_STOPPED
+			}
+		}
+
+		result = append(result, service)
+	}
+
+	return result
+}
---
+++ support/services/services_windows.go
@@ -0,0 +1,18 @@
+// Package services provides methods for collecting information about system services
+package services
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+//                                                                                    //
+//                         Copyright (c) 2024 ESSENTIAL KAOS                          //
+//      Apache License, Version 2.0 <https://www.apache.org/licenses/LICENSE-2.0>     //
+//                                                                                    //
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+import "github.com/essentialkaos/ek/v12/support"
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+// ❗ Collect collect info about services
+func Collect(services ...string) []support.Service {
+	panic("UNSUPPORTED")
+}
