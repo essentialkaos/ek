3018e314a4ba7ce2c79aadf5ba312945363f1e9b
Anton Novojilov <andy@essentialkaos.com>
2024-06-25 17:53:19 +0300

    Improve script for Win

--- .scripts/windows.ps1
+++ .scripts/windows.ps1
@@ -1,13 +1,13 @@
 <# Download dependencies #>
-Function Download-Deps {
+function Get-Deps {
   go get -v golang.org/x/crypto/bcrypt
   go get -v github.com/essentialkaos/depsy
 }
 
 <# Try to install everything #>
-Function Check-Install {
+function Install-All {
   go install ./...
 }
 
-Download-Deps
-Check-Install
+Get-Deps
+Install-All
