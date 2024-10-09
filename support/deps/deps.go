3a42871750abb345ed02bc7cdc8e4837d02b8406
Anton Novojilov <andy@essentialkaos.com>
2024-10-09 12:32:50 +0300

    [support/deps] Improve collecting and filtering dependencies info

--- support/deps/deps.go
+++ support/deps/deps.go
@@ -9,6 +9,9 @@ package deps
 // ////////////////////////////////////////////////////////////////////////////////// //
 
 import (
+	"runtime/debug"
+	"strings"
+
 	"github.com/essentialkaos/ek/v13/support"
 
 	"github.com/essentialkaos/depsy"
@@ -17,16 +20,58 @@ import (
 // ////////////////////////////////////////////////////////////////////////////////// //
 
 // Extract extracts dependencies info from gomod data
-func Extract(gomod []byte) []support.Dep {
+func Extract(gomod []byte, withIndirect ...bool) []support.Dep {
+	if len(withIndirect) > 0 && withIndirect[0] {
+		return filterDeps(depsy.Extract(gomod, true))
+	}
+
+	return filterDeps(depsy.Extract(gomod, false))
+}
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+// filterDeps filters dependencies from gomod using information from bundled build info
+func filterDeps(deps depsy.Dependencies) []support.Dep {
 	var result []support.Dep
 
-	for _, dep := range depsy.Extract(gomod, false) {
-		result = append(result, support.Dep{
+	buildInfo, _ := debug.ReadBuildInfo()
+
+	for _, dep := range deps {
+		depInfo := support.Dep{
 			Version: dep.Version,
 			Path:    dep.PrettyPath(),
 			Extra:   dep.Extra,
-		})
+		}
+
+		if buildInfo != nil {
+			hasDep, version := hasBuiltDep(dep, buildInfo)
+
+			if !hasDep {
+				continue
+			}
+
+			if version != "" && strings.Contains(version, "(") {
+				depInfo.Version = strings.Trim(version, "()")
+			}
+		}
+
+		result = append(result, depInfo)
 	}
 
 	return result
+}
+
+// hasBuiltDep checks if given dependency is present in build info
+func hasBuiltDep(dep depsy.Dependency, buildInfo *debug.BuildInfo) (bool, string) {
+	for _, bDep := range buildInfo.Deps {
+		if bDep.Path == dep.Path {
+			if bDep.Replace != nil {
+				return true, bDep.Replace.Version
+			}
+
+			return true, ""
+		}
+	}
+
+	return false, ""
 }
