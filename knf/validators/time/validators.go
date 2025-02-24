9a1e2fcf314cc633ee4a93269150b1281bd9b323
Anton Novojilov <andy@essentialkaos.com>
2025-02-24 15:40:15 +0300

    [knf/validators/cron] Add new sub-package with KNF validators for time format

---
+++ knf/validators/time/validators.go
@@ -0,0 +1,42 @@
+// Package time provides KNF validators for time-related elements (layouts, formats…)
+package time
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+//                                                                                    //
+//                         Copyright (c) 2025 ESSENTIAL KAOS                          //
+//      Apache License, Version 2.0 <https://www.apache.org/licenses/LICENSE-2.0>     //
+//                                                                                    //
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/essentialkaos/ek/v13/knf"
+	"github.com/essentialkaos/ek/v13/timeutil"
+)
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+var (
+	// Format returns an error if the config property contains an invalid time conversion
+	// format
+	Format = validateFormat
+)
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+func validateFormat(config knf.IConfig, prop string, value any) error {
+	confVal := config.GetS(prop)
+
+	if confVal == "" {
+		return nil
+	}
+
+	if !strings.ContainsRune(timeutil.Format(time.Now(), confVal), '%') {
+		return nil
+	}
+
+	return fmt.Errorf("Property %s contains invalid time format", prop)
+}
---
+++ knf/validators/time/validators_test.go
@@ -0,0 +1,82 @@
+package time
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+//                                                                                    //
+//                         Copyright (c) 2025 ESSENTIAL KAOS                          //
+//      Apache License, Version 2.0 <https://www.apache.org/licenses/LICENSE-2.0>     //
+//                                                                                    //
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+import (
+	"os"
+	"testing"
+
+	"github.com/essentialkaos/ek/v13/knf"
+
+	check "github.com/essentialkaos/check"
+)
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+const _CONFIG_DATA = `
+[time]
+  test1:
+  test2: %D
+  test3: %Y/%m/%d %H:%M:%S %Z
+  test4: %H:%M:%i
+`
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+type ValidatorSuite struct{}
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+var _ = check.Suite(&ValidatorSuite{})
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+func Test(t *testing.T) {
+	check.TestingT(t)
+}
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+func (s *ValidatorSuite) TestFormatValidator(c *check.C) {
+	var err error
+
+	configFile := createConfig(c, _CONFIG_DATA)
+
+	err = knf.Global(configFile)
+	c.Assert(err, check.IsNil)
+
+	errs := knf.Validate([]*knf.Validator{
+		{"time:test1", Format, nil},
+		{"time:test2", Format, nil},
+		{"time:test3", Format, nil},
+	})
+
+	c.Assert(errs, check.HasLen, 0)
+
+	errs = knf.Validate([]*knf.Validator{
+		{"time:test4", Format, nil},
+	})
+
+	c.Assert(errs, check.HasLen, 1)
+
+	c.Assert(errs[0].Error(), check.Equals, `Property time:test4 contains invalid time format`)
+}
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+func createConfig(c *check.C, data string) string {
+	configPath := c.MkDir() + "/config.knf"
+
+	err := os.WriteFile(configPath, []byte(data), 0644)
+
+	if err != nil {
+		c.Fatal(err.Error())
+	}
+
+	return configPath
+}
