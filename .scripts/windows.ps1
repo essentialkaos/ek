2fb58eecb9cc4b22b2226df1fc780ad893325697
Anton Novojilov <andy@essentialkaos.com>
2025-03-29 11:11:48 +0300

    Run tests on Windows

--- .scripts/windows.ps1
+++ .scripts/windows.ps1
@@ -1,3 +1,5 @@
+$env:EK_TEST_PORT = "8080"
+
 <# Download dependencies #>
 function Get-Deps {
   go get -v golang.org/x/crypto/bcrypt
@@ -9,5 +11,11 @@ function Install-All {
   go install ./...
 }
 
+<# Runt tests #>
+function Run-Tests {
+  go test ./...
+}
+
 Get-Deps
 Install-All
+Run-Tests
