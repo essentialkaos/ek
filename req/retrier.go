4ef9d105c75cdd10c72f50c7c02dbaf7a1f84923
Anton Novojilov <andy@essentialkaos.com>
2025-11-20 14:21:21 +0300

    [req] Fix documentation

--- req/retrier.go
+++ req/retrier.go
@@ -53,32 +53,32 @@ func NewRetrier(e ...*Engine) *Retrier {
 
 // ////////////////////////////////////////////////////////////////////////////////// //
 
-// Delete tries to send given request
+// Do tries to send given request
 func (rt *Retrier) Do(r Request, rr Retry) (*Response, error) {
 	return rt.doRequest("", r, rr)
 }
 
-// Delete tries to send GET request
+// Get tries to send GET request
 func (rt *Retrier) Get(r Request, rr Retry) (*Response, error) {
 	return rt.doRequest(GET, r, rr)
 }
 
-// Delete tries to send POST request
+// Post tries to send POST request
 func (rt *Retrier) Post(r Request, rr Retry) (*Response, error) {
 	return rt.doRequest(POST, r, rr)
 }
 
-// Delete tries to send PUT request
+// Put tries to send PUT request
 func (rt *Retrier) Put(r Request, rr Retry) (*Response, error) {
 	return rt.doRequest(PUT, r, rr)
 }
 
-// Delete tries to send HEAD request
+// Head tries to send HEAD request
 func (rt *Retrier) Head(r Request, rr Retry) (*Response, error) {
 	return rt.doRequest(HEAD, r, rr)
 }
 
-// Delete tries to send PATCH request
+// Patch tries to send PATCH request
 func (rt *Retrier) Patch(r Request, rr Retry) (*Response, error) {
 	return rt.doRequest(PATCH, r, rr)
 }
