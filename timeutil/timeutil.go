759ef869e1e291f49ef88cac65324db233cdeaeb
Anton Novojilov <andy@essentialkaos.com>
2026-02-19 00:38:24 +0300

    [timeutil] Move all deprecated method into separate file

--- timeutil/timeutil.go
+++ timeutil/timeutil.go
@@ -524,50 +524,6 @@ func ToDays[N Numeric](days N) time.Duration {
 
 // ////////////////////////////////////////////////////////////////////////////////// //
 
-// SecondsToDuration converts float64 to time.Duration
-//
-// Deprecated: Use [ToSeconds] instead
-func SecondsToDuration(d float64) time.Duration {
-	return ToSeconds(d)
-}
-
-// PrettyDuration returns pretty duration (e.g. 1 hour 45 seconds)
-//
-// Deprecated: Use [Duration.String] instead
-func PrettyDuration(d any) string {
-	return Pretty(d).String()
-}
-
-// PrettyDurationSimple returns simple pretty duration (seconds → minutes → hours → days)
-//
-// Deprecated: Use [Duration.Simple] instead
-func PrettyDurationSimple(d any) string {
-	return Pretty(d).Simple()
-}
-
-// PrettyDurationInDays returns pretty duration in days (e.g. 15 days)
-//
-// Deprecated: Use [Duration.InDays] instead
-func PrettyDurationInDays(d any) string {
-	return Pretty(d).InDays()
-}
-
-// ShortDuration returns pretty short duration (e.g. 1:37)
-//
-// Deprecated: Use [Duration.Short] instead
-func ShortDuration(d any, highPrecision ...bool) string {
-	return Pretty(d).Short(highPrecision...)
-}
-
-// MiniDuration returns formatted value of duration (d/hr/m/s/ms/us/ns)
-//
-// Deprecated: Use [Duration.Mini] instead
-func MiniDuration(d time.Duration, separator ...string) string {
-	return Pretty(d).Mini(separator...)
-}
-
-// ////////////////////////////////////////////////////////////////////////////////// //
-
 // replaceDateTag replaces date tag in format string
 func replaceDateTag(d time.Time, input, output *bytes.Buffer) {
 	r, _, err := input.ReadRune()
