77e5e9362f5221ed67d2819519307da39e2d5c47
Anton Novojilov <andy@essentialkaos.com>
2026-03-08 12:17:18 +0300

    [httputil] Code refactoring

--- httputil/httputil_test.go
+++ httputil/httputil_test.go
@@ -39,6 +39,8 @@ func (s *HTTPUtilSuite) TestRequestParsing(c *C) {
 	c.Assert(req4, NotNil)
 	c.Assert(req5, NotNil)
 
+	c.Assert(GetRequestHost(nil), Equals, "")
+
 	c.Assert(GetRequestHost(req1), Equals, "127.0.0.1")
 	c.Assert(GetRequestHost(req2), Equals, "domain.com")
 	c.Assert(GetRequestHost(req3), Equals, "127.0.0.1")
@@ -64,6 +66,8 @@ func (s *HTTPUtilSuite) TestRemoteParsing(c *C) {
 	req1.RemoteAddr = ""
 	req2.RemoteAddr = "127.0.0.1:12345"
 
+	c.Assert(GetRemoteHost(nil), Equals, "")
+
 	c.Assert(GetRemoteHost(req1), Equals, "")
 	c.Assert(GetRemotePort(req1), Equals, "")
 
@@ -72,48 +76,7 @@ func (s *HTTPUtilSuite) TestRemoteParsing(c *C) {
 }
 
 func (s *HTTPUtilSuite) TestGetDescCode(c *C) {
-	c.Assert(GetDescByCode(100), Equals, "Continue")
-	c.Assert(GetDescByCode(101), Equals, "Switching Protocols")
-	c.Assert(GetDescByCode(200), Equals, "OK")
-	c.Assert(GetDescByCode(201), Equals, "Created")
-	c.Assert(GetDescByCode(202), Equals, "Accepted")
-	c.Assert(GetDescByCode(203), Equals, "Non Authoritative Info")
-	c.Assert(GetDescByCode(204), Equals, "No Content")
-	c.Assert(GetDescByCode(205), Equals, "Reset Content")
-	c.Assert(GetDescByCode(206), Equals, "Partial Content")
-	c.Assert(GetDescByCode(300), Equals, "Multiple Choices")
-	c.Assert(GetDescByCode(301), Equals, "Moved Permanently ")
-	c.Assert(GetDescByCode(302), Equals, "Found")
-	c.Assert(GetDescByCode(303), Equals, "See Other")
-	c.Assert(GetDescByCode(304), Equals, "Not Modified")
-	c.Assert(GetDescByCode(305), Equals, "Use Proxy")
-	c.Assert(GetDescByCode(307), Equals, "Temporary Redirect")
-	c.Assert(GetDescByCode(400), Equals, "Bad Request")
-	c.Assert(GetDescByCode(401), Equals, "Unauthorized")
-	c.Assert(GetDescByCode(402), Equals, "Payment Required")
-	c.Assert(GetDescByCode(403), Equals, "Forbidden")
-	c.Assert(GetDescByCode(404), Equals, "Not Found")
-	c.Assert(GetDescByCode(405), Equals, "Method Not Allowed")
-	c.Assert(GetDescByCode(406), Equals, "Not Acceptable")
-	c.Assert(GetDescByCode(407), Equals, "Proxy Auth Required")
-	c.Assert(GetDescByCode(408), Equals, "Request Timeout")
-	c.Assert(GetDescByCode(409), Equals, "Conflict")
-	c.Assert(GetDescByCode(410), Equals, "Gone")
-	c.Assert(GetDescByCode(411), Equals, "Length Required")
-	c.Assert(GetDescByCode(412), Equals, "Precondition Failed")
-	c.Assert(GetDescByCode(413), Equals, "Request Entity Too Large")
-	c.Assert(GetDescByCode(414), Equals, "Request URI TooLong")
-	c.Assert(GetDescByCode(415), Equals, "Unsupported Media Type")
-	c.Assert(GetDescByCode(416), Equals, "Requested Range Not Satisfiable")
-	c.Assert(GetDescByCode(417), Equals, "Expectation Failed")
-	c.Assert(GetDescByCode(418), Equals, "Teapot")
-	c.Assert(GetDescByCode(500), Equals, "Internal Server Error")
-	c.Assert(GetDescByCode(501), Equals, "Not Implemented")
-	c.Assert(GetDescByCode(502), Equals, "Bad Gateway")
-	c.Assert(GetDescByCode(503), Equals, "Service Unavailable")
-	c.Assert(GetDescByCode(504), Equals, "Gateway Timeout")
-	c.Assert(GetDescByCode(505), Equals, "HTTP Version Not Supported")
-	c.Assert(GetDescByCode(999), Equals, "Unknown")
+	c.Assert(GetDescByCode(999), Equals, "")
 }
 
 func (s *HTTPUtilSuite) TestURLCheck(c *C) {
