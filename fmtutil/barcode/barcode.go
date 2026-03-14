f1cd0c875db1011036384a51dc4e6a46af62091c
Anton Novojilov <andy@essentialkaos.com>
2026-03-14 13:40:57 +0300

    [fmtutil/barcode] Code refactoring

--- fmtutil/barcode/barcode.go
+++ fmtutil/barcode/barcode.go
@@ -11,58 +11,73 @@ package barcode
 import (
 	"fmt"
 	"hash/crc32"
+	"strings"
 )
 
 // ////////////////////////////////////////////////////////////////////////////////// //
 
 // dots is used for dots generation
 var dots = []string{
-	"⠮", "⠉", "⠒", "⠤", "⠝", "⠳", "⠪", "⠭",
-	"⠕", "⠔", "⠮", "⠌", "⠡", "⠢", "⠜", "⠵",
+	"⠮", "⠉", "⠒", "⠤", "⠝", "⠳", "⠪", "⠭", "⠕", "⠔", "⠮", "⠌", "⠡", "⠢", "⠜", "⠵",
 }
 
 // lines is used for lines generation
 var lines = []string{
-	"╬", "╪", "╩", "╧", "╦", "╤", "╣", "╡",
-	"╠", "╞", "╝", "╛", "╚", "╘", "╕", "╔",
+	"╬", "╪", "╩", "╧", "╦", "╤", "╣", "╡", "╠", "╞", "╝", "╛", "╚", "╘", "╕", "╔",
 }
 
 // boxes is used for boxes generation
 var boxes = []string{
-	"▁", "▂", "▃", "▄", "▅", "▆", "▇", "█",
-	"█", "▇", "▆", "▅", "▄", "▃", "▂", "▁",
+	"▁", "▂", "▃", "▄", "▅", "▆", "▇", "█", "█", "▇", "▆", "▅", "▄", "▃", "▂", "▁",
 }
 
 // colors contains ASCII color codes
 var colors = []int{
-	1, 2, 3, 4, 5, 6, 9, 10,
-	11, 12, 13, 14, 2, 12, 5, 14,
+	1, 2, 3, 4, 5, 6, 9, 10, 11, 12, 13, 14, 2, 12, 5, 14,
 }
 
 // ////////////////////////////////////////////////////////////////////////////////// //
 
-// Dots generates dots
+// Dots returns a 16-symbol ANSI-colored barcode string composed of braille dot
+// characters. The glyph and foreground color of each symbol are derived from the
+// CRC32-IEEE checksum of data, so any two distinct byte slices will, with high
+// probability, produce visually different barcodes.
+//
+// If data is empty, Dots returns a uniform 16-character red error indicator
+// ("⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒") to signal that no meaningful input was provided.
 func Dots(data []byte) string {
 	if len(data) == 0 {
-		return fmt.Sprintf("\033[38;5;1m%s\033[0m", "⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒")
+		return "\033[38;5;1m⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒\033[0m"
 	}
 
 	return generate(data, dots)
 }
 
-// Lines generates lines
+// Lines returns a 16-symbol ANSI-colored barcode string composed of box-drawing
+// line characters. The glyph and foreground color of each symbol are derived
+// from the CRC32-IEEE checksum of data, providing a consistent visual
+// fingerprint for any unique byte sequence.
+//
+// If data is empty, Lines returns a uniform 16-character red error indicator
+// ("╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬") to signal that no meaningful input was provided.
 func Lines(data []byte) string {
 	if len(data) == 0 {
-		return fmt.Sprintf("\033[38;5;1m%s\033[0m", "╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬")
+		return "\033[38;5;1m╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬\033[0m"
 	}
 
 	return generate(data, lines)
 }
 
-// Boxes generates boxes
+// Boxes returns a 16-symbol ANSI-colored barcode string composed of block
+// element characters. The fill level and foreground color of each symbol are
+// derived from the CRC32-IEEE checksum of data, giving a compact visual hash
+// that is easy to compare at a glance in a terminal.
+//
+// If data is empty, Boxes returns a uniform 16-character red error indicator
+// ("████████████████") to signal that no meaningful input was provided.
 func Boxes(data []byte) string {
 	if len(data) == 0 {
-		return fmt.Sprintf("\033[38;5;1m%s\033[0m", "████████████████")
+		return "\033[38;5;1m████████████████\033[0m"
 	}
 
 	return generate(data, boxes)
@@ -70,20 +85,50 @@ func Boxes(data []byte) string {
 
 // ////////////////////////////////////////////////////////////////////////////////// //
 
-// generate generates barcode for given data
+// generate computes the CRC32-IEEE checksum of data, splits it into four bytes
+// (most-significant first), and concatenates the colored symbol segment for
+// each byte into a single 16-symbol ANSI string
 func generate(data []byte, alpha []string) string {
+	var result strings.Builder
+
 	c := crc32.ChecksumIEEE(data)
+
 	c1, c2, c3, c4 := (c>>24)&0xFF, (c>>16)&0xFF, (c>>8)&0xFF, c&0xFF
-	return barcode(c1, alpha) + barcode(c2, alpha) + barcode(c3, alpha) + barcode(c4, alpha)
+
+	result.WriteString(barcode(c1, alpha))
+	result.WriteString(barcode(c2, alpha))
+	result.WriteString(barcode(c3, alpha))
+	result.WriteString(barcode(c4, alpha))
+
+	return result.String()
 }
 
-// barcode generates barcode part
+// barcode maps nibble-range indexes extracted from byte c to ANSI-colored
+// terminal symbols using the colors and alpha lookup tables
 func barcode(c uint32, alpha []string) string {
-	var result string
+	var result strings.Builder
 
-	for _, i := range []uint32{c >> 4 & 0xF, c >> 2 & 0xF, c >> 1 & 0xF, c & 0xF} {
-		result += fmt.Sprintf("\033[38;5;%dm%s\033[0m", colors[i], alpha[i])
+	lastColor := -1
+
+	for _, i := range []uint32{(c >> 6) & 0x3, (c >> 4) & 0x3, (c >> 2) & 0x3, c & 0x3} {
+		color := colors[i]
+
+		if color != lastColor {
+			if lastColor != -1 {
+				result.WriteString("\033[0m")
+			}
+
+			fmt.Fprintf(&result, "\033[38;5;%dm", color)
+
+			lastColor = color
+		}
+
+		result.WriteString(alpha[i])
 	}
 
-	return result
+	if lastColor != -1 {
+		result.WriteString("\033[0m")
+	}
+
+	return result.String()
 }
--- fmtutil/barcode/barcode_test.go
+++ fmtutil/barcode/barcode_test.go
@@ -27,15 +27,15 @@ var _ = Suite(&BarcodeSuite{})
 
 func (s *BarcodeSuite) TestDots(c *C) {
 	c.Assert(Dots([]byte("")), Equals, "\x1b[38;5;1m⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒⠒\x1b[0m")
-	c.Assert(Dots([]byte("Test1234!")), Equals, "\x1b[38;5;1m⠮\x1b[0m\x1b[38;5;3m⠒\x1b[0m\x1b[38;5;6m⠳\x1b[0m\x1b[38;5;14m⠌\x1b[0m\x1b[38;5;11m⠕\x1b[0m\x1b[38;5;1m⠮\x1b[0m\x1b[38;5;2m⠉\x1b[0m\x1b[38;5;3m⠒\x1b[0m\x1b[38;5;4m⠤\x1b[0m\x1b[38;5;12m⠢\x1b[0m\x1b[38;5;14m⠌\x1b[0m\x1b[38;5;9m⠪\x1b[0m\x1b[38;5;2m⠉\x1b[0m\x1b[38;5;6m⠳\x1b[0m\x1b[38;5;14m⠌\x1b[0m\x1b[38;5;9m⠪\x1b[0m")
+	c.Assert(Dots([]byte("Test1234!")), Equals, "\x1b[38;5;1m⠮⠮\x1b[0m\x1b[38;5;3m⠒\x1b[0m\x1b[38;5;4m⠤\x1b[0m\x1b[38;5;3m⠒\x1b[0m\x1b[38;5;1m⠮⠮\x1b[0m\x1b[38;5;3m⠒\x1b[0m\x1b[38;5;1m⠮\x1b[0m\x1b[38;5;4m⠤\x1b[0m\x1b[38;5;2m⠉\x1b[0m\x1b[38;5;3m⠒\x1b[0m\x1b[38;5;1m⠮\x1b[0m\x1b[38;5;2m⠉⠉\x1b[0m\x1b[38;5;3m⠒\x1b[0m")
 }
 
 func (s *BarcodeSuite) TestLines(c *C) {
 	c.Assert(Lines([]byte("")), Equals, "\x1b[38;5;1m╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬\x1b[0m")
-	c.Assert(Lines([]byte("Test1234!")), Equals, "\x1b[38;5;1m╬\x1b[0m\x1b[38;5;3m╩\x1b[0m\x1b[38;5;6m╤\x1b[0m\x1b[38;5;14m╛\x1b[0m\x1b[38;5;11m╠\x1b[0m\x1b[38;5;1m╬\x1b[0m\x1b[38;5;2m╪\x1b[0m\x1b[38;5;3m╩\x1b[0m\x1b[38;5;4m╧\x1b[0m\x1b[38;5;12m╘\x1b[0m\x1b[38;5;14m╛\x1b[0m\x1b[38;5;9m╣\x1b[0m\x1b[38;5;2m╪\x1b[0m\x1b[38;5;6m╤\x1b[0m\x1b[38;5;14m╛\x1b[0m\x1b[38;5;9m╣\x1b[0m")
+	c.Assert(Lines([]byte("Test1234!")), Equals, "\x1b[38;5;1m╬╬\x1b[0m\x1b[38;5;3m╩\x1b[0m\x1b[38;5;4m╧\x1b[0m\x1b[38;5;3m╩\x1b[0m\x1b[38;5;1m╬╬\x1b[0m\x1b[38;5;3m╩\x1b[0m\x1b[38;5;1m╬\x1b[0m\x1b[38;5;4m╧\x1b[0m\x1b[38;5;2m╪\x1b[0m\x1b[38;5;3m╩\x1b[0m\x1b[38;5;1m╬\x1b[0m\x1b[38;5;2m╪╪\x1b[0m\x1b[38;5;3m╩\x1b[0m")
 }
 
 func (s *BarcodeSuite) TestBoxes(c *C) {
 	c.Assert(Boxes([]byte("")), Equals, "\x1b[38;5;1m████████████████\x1b[0m")
-	c.Assert(Boxes([]byte("Test1234!")), Equals, "\x1b[38;5;1m▁\x1b[0m\x1b[38;5;3m▃\x1b[0m\x1b[38;5;6m▆\x1b[0m\x1b[38;5;14m▅\x1b[0m\x1b[38;5;11m█\x1b[0m\x1b[38;5;1m▁\x1b[0m\x1b[38;5;2m▂\x1b[0m\x1b[38;5;3m▃\x1b[0m\x1b[38;5;4m▄\x1b[0m\x1b[38;5;12m▃\x1b[0m\x1b[38;5;14m▅\x1b[0m\x1b[38;5;9m▇\x1b[0m\x1b[38;5;2m▂\x1b[0m\x1b[38;5;6m▆\x1b[0m\x1b[38;5;14m▅\x1b[0m\x1b[38;5;9m▇\x1b[0m")
+	c.Assert(Boxes([]byte("Test1234!")), Equals, "\x1b[38;5;1m▁▁\x1b[0m\x1b[38;5;3m▃\x1b[0m\x1b[38;5;4m▄\x1b[0m\x1b[38;5;3m▃\x1b[0m\x1b[38;5;1m▁▁\x1b[0m\x1b[38;5;3m▃\x1b[0m\x1b[38;5;1m▁\x1b[0m\x1b[38;5;4m▄\x1b[0m\x1b[38;5;2m▂\x1b[0m\x1b[38;5;3m▃\x1b[0m\x1b[38;5;1m▁\x1b[0m\x1b[38;5;2m▂▂\x1b[0m\x1b[38;5;3m▃\x1b[0m")
 }
