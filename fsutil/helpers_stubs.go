f04fdd0f73bdab6adcc1301a9099ba1c6911f735
Anton Novojilov <andy@essentialkaos.com>
2026-05-21 00:22:33 +0300

    [fsutil] Fix helpers stubs

--- fsutil/helpers_stubs.go
+++ fsutil/helpers_stubs.go
@@ -18,30 +18,30 @@ import (
 // ❗ CopyFile copies file using bufio
 func CopyFile(from, to string, perms ...os.FileMode) error {
 	panic("UNSUPPORTED")
-	return nil
 }
 
 // ❗ CopyAttr copies attributes (mode, ownership, timestamps) from one object
 // (file or directory) to another
 func CopyAttr(from, to string) error {
 	panic("UNSUPPORTED")
-	return nil
 }
 
 // ❗ MoveFile moves file
 func MoveFile(from, to string, perms ...os.FileMode) error {
 	panic("UNSUPPORTED")
-	return nil
 }
 
 // ❗ CopyDir copies directory content recursively to target directory
 func CopyDir(from, to string) error {
 	panic("UNSUPPORTED")
-	return nil
 }
 
 // ❗ TouchFile creates empty file
 func TouchFile(path string, perm os.FileMode) error {
 	panic("UNSUPPORTED")
-	return nil
+}
+
+// ❗ CountLines returns the number of newline-delimited lines in the given file
+func CountLines(file string) (int, error) {
+	panic("UNSUPPORTED")
 }
